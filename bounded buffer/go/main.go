c902568d17166bd929a9115db647af09bd1b468a
Truls Sæther <truls_saether@Trulss-MacBook-Pro.local>
2025-04-01 20:10:27 +0200

    FINAL

--- bounded buffer/go/main.go
+++ bounded buffer/go/main.go
@@ -1,39 +1,37 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+func producer(buffer chan int) {
 
-func producer(buffer chan int){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-        buffer <- i // <-- changed
-    }
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		buffer <- i
+	}
 
 }
 
-func consumer(buffer chan int){
+func consumer(buffer chan int) {
 
-    time.Sleep(1 * time.Second)
-    for {
-        value := <-buffer // <-- changed
-        fmt.Printf("[consumer]: %d\n", value)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+	time.Sleep(1 * time.Second)
+	for {
+		value := <-buffer
+		fmt.Printf("[consumer]: %d\n", value)
+		time.Sleep(50 * time.Millisecond)
+	}
+
 }
 
+func main() {
 
-func main(){
-    
-    buffer := make(chan int, 5)
-    
-    go consumer(buffer)
-    go producer(buffer)
-    
-    select {}
+	buffer := make(chan int, 5)
+
+	go consumer(buffer)
+	go producer(buffer)
+
+	select {}
 }
